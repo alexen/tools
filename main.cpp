d83205130960860ecf45b249d7093e6220d5ac96
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2017-09-28 23:32:32 +0300

    main: Использование новых функций для отправки HTTP-запросов

--- main.cpp
+++ main.cpp
@@ -6,22 +6,57 @@
 ///
 
 #include <cstdio>
+#include <list>
+#include <memory>
 #include <stdexcept>
 #include <iostream>
 #include <boost/exception/diagnostic_information.hpp>
 
-#include <curl_tools/curl_raii.h>
+#include <curl_tools/utilities.h>
 
 
 int main( int argc, char** argv )
 {
+     using namespace curl::tools;
+
      try
      {
-          const auto url = argc > 1 ? argv[ 1 ] : "www.yandex.ru";
+          const auto url = "http://127.0.0.1:9999";
 
-          curl::tools::raii::CurlRaii{ url }
-               .setResponseStream( std::cout )
-               .perform();
+          const auto curl = utilities::makeCurl();
+
+          std::cout << "Request:\n";
+
+          auto headers =
+               utilities::makeHeadersList({
+                    { "X-Extra-Header", "extra header value" }
+               });
+
+          utilities::updateHeadersList( headers, {
+               { "X-Another-Extra-Header", "some another extra value" }
+          });
+
+          const std::string body = ""
+               "{\n"
+               "\t\"message\": \"My Bonny is over the ocean...\"\n"
+               "}\n";
+
+          utilities::updateHeadersList( headers, {
+               { "Content-Type", "application/json" }
+          });
+
+          utilities::setBasicAuth( curl, "alexen", "111111" );
+          utilities::setUserAgent( curl, "Self-Writed-Curl-Functions" );
+
+          const auto status = utilities::post(
+               curl,
+               headers,
+               url,
+               body.data(),
+               body.size(),
+               std::cout );
+
+          std::cout << "\nStatus: " << status << "\n";
      }
      catch( const std::exception& e )
      {

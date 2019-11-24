65599273b6067f7f68cde26c148e26b35b121cb9
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2019-11-24 22:19:36 +0300

    network/utils: Реализованы тесты функций udp::send() для udp::Connection

--- network/test/utils_test.cpp
+++ network/test/utils_test.cpp
@@ -82,3 +82,53 @@ TEST( NetworkUdp, ValidConnectionToIpV6 )
 {
      ASSERT_NO_THROW( tools::network::udp::connect( aux::ipv6::localhost, aux::port ) );
 }
+
+
+TEST( NetworkUdp, InvalidSendCausesException )
+{
+     tools::network::udp::Connection connection;
+     ASSERT_THROW( tools::network::udp::send( connection, "", 0u ), std::system_error );
+}
+
+
+TEST( NetworkUdp, InvalidSendSetsErrorCode )
+{
+     std::error_code error;
+     tools::network::udp::Connection connection;
+     ASSERT_NO_THROW( tools::network::udp::send( connection, "", 0u, error ) );
+     ASSERT_TRUE( error );
+     ASSERT_EQ( std::error_code( ENOTSOCK, std::system_category() ), error );
+}
+
+
+TEST( NetworkUdp, SendOverOneConnection )
+{
+     static const std::string lorem =
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod "
+          "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, "
+          "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo "
+          "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse "
+          "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non "
+          "proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+     std::error_code error;
+     const auto connection = tools::network::udp::connect( aux::hostname, aux::port, error );
+     ASSERT_TRUE( !error );
+     ASSERT_EQ( 0u, tools::network::udp::send( connection, "", 0 ) );
+     ASSERT_EQ( 7u, tools::network::udp::send( connection, "1234567", 7 ) );
+     ASSERT_EQ( lorem.size(), tools::network::udp::send( connection, lorem ) );
+}
+
+
+TEST( NetworkUdp, SendOverSeveralConnections )
+{
+     static const std::string lorem =
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod "
+          "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, "
+          "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo "
+          "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse "
+          "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non "
+          "proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+     ASSERT_EQ( 0u, tools::network::udp::send( aux::hostname, aux::port, "", 0 ) );
+     ASSERT_EQ( 9u, tools::network::udp::send( aux::ipv4::localhost, aux::port, "123456789", 9u ) );
+     ASSERT_EQ( lorem.size(), tools::network::udp::send( aux::ipv6::localhost, aux::port, lorem ) );
+}
